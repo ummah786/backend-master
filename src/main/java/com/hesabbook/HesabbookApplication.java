8d50cbd3d260d5545ca8a3d62e6add40f4ae2ad3
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2024-03-25 07:43:44 +0530

    21-March

--- src/main/java/com/hesabbook/HesabbookApplication.java
+++ src/main/java/com/hesabbook/HesabbookApplication.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,6 +40,14 @@ public class HesabbookApplication {
     private UserService userService;
     @Autowired
     private UserServiceImpl userServiceimpl;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/message")
+    public void handleIncomingMessage(@Payload String message) {
+        messagingTemplate.convertAndSend("/topic/messages", message);
+    }
 
     @PostMapping("/login")
     public BusinessResponse login(@RequestBody User user) {
--- src/main/java/com/hesabbook/service/account/ManageUserService.java
+++ src/main/java/com/hesabbook/service/account/ManageUserService.java
@@ -2,9 +2,12 @@ package com.hesabbook.service.account;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import com.hesabbook.entity.account.ManageUsers;
+import com.hesabbook.entity.account.User;
 import com.hesabbook.repository.ManageUsersRepository;
+import com.hesabbook.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +16,43 @@ import org.springframework.stereotype.Service;
 public class ManageUserService {
     @Autowired
     ManageUsersRepository manageUsersRepository;
+    @Autowired
+    UserService userService;
 
     public ManageUsers save(ManageUsers ManageUsers) {
+        Random random = new Random();
+        if(ManageUsers.getName()!=null){
+            int randomNumber = random.nextInt(90000) + 10000;
+            ManageUsers.setSecondary_user_id(ManageUsers.getName().trim()+randomNumber);
+        }else if(ManageUsers.getMobileNumber()!=null){
+            int randomNumber = random.nextInt(900) + 100;
+            ManageUsers.setSecondary_user_id(ManageUsers.getMobileNumber().trim()+randomNumber);
+        }else if(ManageUsers.getEmailAddress()!=null){
+            int randomNumber = random.nextInt(900) + 100;
+            ManageUsers.setSecondary_user_id(ManageUsers.getMobileNumber().trim()+randomNumber);
+        }else {
+            int randomData = random.nextInt(90000000) + 10000000;
+            ManageUsers.setSecondary_user_id(String.valueOf(randomData));
+        }
+        saveIntoUser(ManageUsers);
         return manageUsersRepository.save(ManageUsers);
     }
 
     public ManageUsers update(ManageUsers entity) {
         return manageUsersRepository.save(entity);
+    }
+
+    private void saveIntoUser(ManageUsers manageUsers) {
+
+        User user = new User();
+        user.setFirstName(manageUsers.getName());
+        user.setEmail(manageUsers.getEmailAddress());
+        user.setMobileNumber(manageUsers.getMobileNumber());
+        user.setPrimary_user_id(manageUsers.getPrimary_user_id());
+        user.setRole(manageUsers.getRole());
+        user.setSecondary_user_id(manageUsers.getSecondary_user_id());
+        user.setTempPassword(manageUsers.getTempPassword());
+        userService.save(user);
     }
 
     public void delete(ManageUsers entity) {
