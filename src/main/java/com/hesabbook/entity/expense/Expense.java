1e44f80592495765b82615a646e9b4f1b23b4ab9
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2024-04-09 07:12:51 +0530

    27-March

--- src/main/java/com/hesabbook/entity/expense/Expense.java
+++ src/main/java/com/hesabbook/entity/expense/Expense.java
@@ -2,14 +2,13 @@ package com.hesabbook.entity.expense;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
