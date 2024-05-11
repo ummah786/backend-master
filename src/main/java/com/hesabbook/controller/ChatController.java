80a7431e296bdd3db7fd98a2c6d31cb035fa8b97
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2024-05-11 16:39:52 +0530

    11-May

--- src/main/java/com/hesabbook/controller/ChatController.java
+++ src/main/java/com/hesabbook/controller/ChatController.java
@@ -2,19 +2,35 @@ package com.hesabbook.controller;
 
 import com.hesabbook.dto.Message;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class ChatController {
-
-    @MessageMapping("/hello")
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
+ /*   @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Message sendMessage(Message message) {
+
+        // Send the message to the user-specific topic
+        //   messagingTemplate.convertAndSendToUser(recipientUserId, "/queue/messages", message);
+
         return message;
+    }*/
+
+    @MessageMapping("/hello")
+    public void sendMessageValue(Message message) {
+        //  messagingTemplate.convertAndSendToUser(recipientUserId, "/topic/greetings", message);
+        String destination = "/topic/greetings/"+message.getReceiver();
+        messagingTemplate.convertAndSend(destination, message);
+        messagingTemplate.convertAndSend("/topic/greetings", message);
+        messagingTemplate.convertAndSend("/topic/greetings/asif", message);
     }
 
     @MessageMapping("/signal")
