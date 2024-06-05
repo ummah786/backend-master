7e18b708c90b0c8c18ee19109b810c51ab1c2de1
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2024-06-06 01:42:52 +0530

    6-June

--- src/main/java/com/hesabbook/controller/BankAccountController.java
+++ src/main/java/com/hesabbook/controller/BankAccountController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/bankaccounts")
+@RequestMapping("/hesabbook/bank/account")
 public class BankAccountController {
     @Autowired
     private BankAccountService bankAccountService;
